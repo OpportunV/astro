f59f092039303322bbdb93b23272adfcc7405987
OpportunV <Vadya.95@mail.ru>
2019-12-05 22:28:15 +0300

    small enhancement

--- run.py
+++ run.py
@@ -4,4 +4,6 @@ from view import *
 app.register_blueprint(tasks, url_prefix='/tasks')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    # from waitress import serve
+    # serve(app, host='127.0.0.1', port=5000)

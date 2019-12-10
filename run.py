5b35243cd1cc7fcc6117dc63ed0716ac6b65cdef
OpportunV <Vadya.95@mail.ru>
2019-12-10 11:17:33 +0300

    errors

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from tasks.blueprint import tasks
 from users.view import users
+from errors.view import errors
 from view import *
 
 app.register_blueprint(tasks, url_prefix='/tasks')
 app.register_blueprint(users)
+app.register_blueprint(errors)
 
 if __name__ == '__main__':
     app.run(debug=True)
